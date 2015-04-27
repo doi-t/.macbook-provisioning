ba586034ef3b4d94acd226b8f29a15e273f1ae79
Toshiya Doi <doi.t.npdp@gmail.com>
2015-04-27 22:51:46 +0900

    add .zshrc.local & .zshrc.mac

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sed -ie 's/ZSH_THEME=.*/ZSH_THEME=\"steeef\"/'  ~/.zshrc
-cat './direnv.txt' >> ~/.zshrc
+zshrc='~/.zshrc'
+sed -ie 's/ZSH_THEME=.*/ZSH_THEME=\"steeef\"/' $zshrc
+echo '[ -f ~/.zshrc.local ] && source ~/.zshrc.local' >> $zshrc
+echo '[ -f ~/.zshrc.mac ] && source ~/.zshrc.mac' >> $zshrc
