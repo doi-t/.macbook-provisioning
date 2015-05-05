d8cb9b7f5c551b1aa92e5f9008678030ac91d190
Toshiya Doi <doi.t.npdp@gmail.com>
2015-05-06 00:22:42 +0900

    remove a source command for .zshrc.mac

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -2,4 +2,3 @@
 zshrc=~/.zshrc
 sed -ie 's/ZSH_THEME=.*/ZSH_THEME=\"steeef\"/' $zshrc
 echo '[ -f ~/.zshrc.local ] && source ~/.zshrc.local' >> $zshrc
-echo '[ -f ~/.zshrc.mac ] && source ~/.zshrc.mac' >> $zshrc
