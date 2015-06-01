420c1c1d56151d2270607a19bdb626afa7a1f4fa
Toshiya Doi <doi.t.npdp@gmail.com>
2015-06-01 23:36:16 +0900

    add command list for rbenv

---
+++ manual-commands.sh
@@ -0,0 +1,8 @@
+cat << EOF
+rbenv --version             # => version check
+rbenv install --list        # => display the version list
+rbenv install [version]     # => install ruby
+rbenv rehash                # => reload rbenv
+rbenv global [version]      # => setuup the default ruby version
+sudo gem update --system
+EOF
