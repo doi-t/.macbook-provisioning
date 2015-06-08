8ce483c2757fd5001b597368d9bd2494119febc5
toshiya.doi <toshiya.doi@hde.co.jp>
2015-06-08 10:20:03 +0900

    remove a recipe of vagrant plugin

--- manual-commands.sh
+++ manual-commands.sh
@@ -5,4 +5,6 @@ rbenv install [version]     # => install ruby
 rbenv rehash                # => reload rbenv
 rbenv global [version]      # => setuup the default ruby version
 sudo gem update --system
+# install vagrant plugin
+vagrant plugin install sahara vagrant-cachier vagrant-exec vagrant-proxyconf vagrant-vbguest vagrant-omnibus
 EOF
