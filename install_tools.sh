2e192b511f80455685a685b0d6e9c1e69b7e8140
Toshiya Doi <doi.t.npdp@gmail.com>
2017-12-17 15:22:51 +0900

    update tools related python and kubernetes

--- install_tools.sh
+++ install_tools.sh
@@ -1,16 +1,24 @@
-cat << EOF
-rbenv --version             # => version check
+#!/bin/bash
+
+# Ruby
 rbenv install --list        # => display the version list
 rbenv install [version]     # => install ruby
 rbenv rehash                # => reload rbenv
 rbenv global [version]      # => setuup the default ruby version
 sudo gem update --system
+
 # install vagrant plugin
 vagrant plugin install sahara vagrant-cachier vagrant-exec vagrant-proxyconf vagrant-vbguest vagrant-omnibus
+
 # python tools
-easy_install pip
 pip install awscli
 pip install virtualenv
-# setup iteam2 for solarized http://qiita.com/ApricotHoney/items/f83e4aa9c1cd83755c68
-# choose Ricty in iTerm2 preferences: Performances > Profiles > Text
-EOF
+
+# terminal
+echo 'setup iTerm2 Color Presets: https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized'
+echo 'choose Ricty in iTerm2 preferences: Performances > Profiles > Text'
+
+# kubernetes
+echo 'Download the Google Cloud SDK from https://cloud.google.com/sdk/docs/quickstart-mac-os-x'
+sh /usr/local/bin/google-cloud-sdk/install.sh
+gcloud components update
