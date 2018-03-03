ce1d8754c2d642431b974564dc7ec46f9e72c5c6
Toshiya Doi <doi.t.npdp@gmail.com>
2018-03-03 19:50:49 +0900

    add custom fzf commands

---
+++ dotfzf.zsh
@@ -0,0 +1,8 @@
+# fh - repeat history
+fh() {
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+fhe() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
