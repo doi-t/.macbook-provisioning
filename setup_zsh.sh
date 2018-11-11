da1dcd7691213bdc064bb59f0c0bb96e88fc9515
Toshiya Doi <doi.t.npdp@gmail.com>
2018-11-11 19:55:28 +0900

    migrate zsh related config to terminal role

--- setup_zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-zshrc=~/.zshrc
-sed -ie 's/ZSH_THEME=.*/ZSH_THEME=\"steeef\"/' $zshrc
-echo '[ -f ~/.zshrc.local ] && source ~/.zshrc.local' >> $zshrc
-echo '[ -f ~/.zshrc.credentials ] && source ~/.zshrc.credentials' >> $zshrc
